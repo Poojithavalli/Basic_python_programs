3b37ef8a0bd67ecc15cfaff07071b8ae0e52152f
Poojitha Valli P <135353858+Poojithavalli@users.noreply.github.com>
2024-08-12 21:01:17 +0530

    Add files via upload

---
+++ multiple_2.py
@@ -0,0 +1,8 @@
+num=int(input("enter a number to check whether it is multiple of two or not : "))
+if num/2==0:
+    print(f"{num} is a multiple of 2")
+else:
+    print(f"{num} is not a multiple of 2")
+
+
+
